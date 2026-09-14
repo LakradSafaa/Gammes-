42eb6588c9fe42ac885b8ff22ef21772e3e6481b
LakradSafaa <125573861+LakradSafaa@users.noreply.github.com>
2026-09-14 18:52:38 +0100

    Delete frontend/src/pages/gammes directory

--- frontend/src/pages/gammes/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as GammesList } from "./GammesList";
-export { default as GammeCreate } from "./GammeCreate";
-export { default as GammeDetail } from "./GammeDetail";
-export { default as GammeEdit } from "./GammeEdit";
-export { default as QrPublic } from "./QrPublic";
