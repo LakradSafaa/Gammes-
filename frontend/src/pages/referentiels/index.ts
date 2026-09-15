db8377da403a095936d6a61c4f4ea29660560887
LakradSafaa <125573861+LakradSafaa@users.noreply.github.com>
2026-09-15 21:17:52 +0100

    Delete frontend/src directory

--- frontend/src/pages/referentiels/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as EPIPage } from "./EPIPage";
-export { default as RisquesPage } from "./RisquesPage";
-export { default as OutillagesPage } from "./OutillagesPage";
-export { default as PiecesPage } from "./PiecesPage";
