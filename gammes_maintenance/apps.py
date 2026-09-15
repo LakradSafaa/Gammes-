7cdd1a76daea90564ec80d97a9d3a714ed9f62d1
LakradSafaa <125573861+LakradSafaa@users.noreply.github.com>
2026-09-15 21:14:00 +0100

    Delete gammes_maintenance directory

--- gammes_maintenance/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class GammesMaintenanceConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "gammes_maintenance"
-
-    def ready(self):
-        import gammes_maintenance.signals  # noqa: F401
--- gammes_maintenance/signals.py
+++
@@ -1,9 +0,0 @@
-"""
-Signals de l'application gammes_maintenance.
-
-Aucun signal n'est utilisé actuellement.
-
-L'ancien signal UserProfile a été désactivé parce que
-le modèle UserProfile ne fait pas partie du schéma
-Supabase actuel.
-"""
